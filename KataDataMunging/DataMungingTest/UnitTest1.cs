e5baa0fb0ac4e3a0b59f72e4f5b7adaa7ee61a52
huwyss <hansueli.wyss@gmail.com>
2017-11-19 13:13:22 +0100

    Adding old katas...

---
+++ KataDataMunging/DataMungingTest/UnitTest1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KataDataMunging;
+
+namespace DataMungingTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void returnsTheDayOfASingleLine()
+        {
+            //arrange
+            string singleLine = "   4  77    59    68          51.1       0.00         110  9.1 130  12  8.6  62 40 1021.1";
+
+            // act
+            int day = DayFinder.dayFromLine(singleLine);
+
+            // assert
+            Assert.AreEqual(4, day);
+        }
+
+        [TestMethod]
+        public void returnsTheTemperatureDifferenceOfASingleLine()
+        {
+            //arrange
+            string singleLine = "   4  77    59    68          51.1       0.00         110  9.1 130  12  8.6  62 40 1021.1";
+
+            // act
+            int difference = DayFinder.temperatureDifferenceFromLine(singleLine);
+
+            // assert
+            Assert.AreEqual(18, difference);
+        }
+
+        [TestMethod]
+        public void returnsTheLastDayOfATwoDayMonth()
+        {
+            //arrange
+            string fileContent = "   4  77    59    68          51.1       0.00         110  9.1 130  12  8.6  62 40 1021.1\n"+
+                                 "   5  90    89    78          68.3       0.00 TFH     220  8.3 260  12  6.9  84 55 1014.4";
+
+            // act
+            int day = DayFinder.dayWithMinimalTemperatureRange(fileContent);
+
+            // assert
+            Assert.AreEqual(5, day);
+        }
+
+        [TestMethod]
+        public void returnsTheFirstDayOfATwoDayMonth()
+        {
+            //arrange
+            string fileContent = "   4  77    76    68          51.1       0.00         110  9.1 130  12  8.6  62 40 1021.1\n" +
+                                 "   5  90    88    78          68.3       0.00 TFH     220  8.3 260  12  6.9  84 55 1014.4";
+
+            // act
+            int day = DayFinder.dayWithMinimalTemperatureRange(fileContent);
+
+            // assert
+            Assert.AreEqual(4, day);
+        }
+
+        [TestMethod]
+        public void returnsTheFirstDayOfAThreeDayMonth()
+        {
+            //arrange
+            string fileContent = "   6  81    61    71          63.7       0.00 RFH     030  6.2 030  13  9.7  93 60 1012.7\n" +
+                                 "   7  73    72    65          53.0       0.00 RF      050  9.5 050  17  5.3  90 48 1021.8\n" +
+                                 "   8  75    54    65          50.0       0.00 FH      160  4.2 150  10  2.6  93 41 1026.3";
+
+            // act
+            int day = DayFinder.dayWithMinimalTemperatureRange(fileContent);
+
+            // assert
+            Assert.AreEqual(7, day);
+        }
+    }
+}
---
+++ KataDataMunging/KataDataMunging/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataDataMunging
+{
+    /*
+     * first(minTuple(tupleStream(readFile())))
+     * 
+     * readFile() |> tupleStream() |> minTuple() |> first()
+     */
+
+    public class DayFinder
+    {
+        private static int numericElement(string input, int index)
+        {
+            return int.Parse(input.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[index]);
+        }
+
+        public static int dayFromLine(string input)
+        {
+            return numericElement(input, 0);
+        }
+
+        public static int temperatureDifferenceFromLine(string input)
+        {
+            return numericElement(input, 1) - numericElement(input, 2);
+        }
+
+        public static int dayWithMinimalTemperatureRange(string fileContent)
+        {
+            int dayOfSmallestDiff = 0;
+            int smallestDiff = 1000000;
+            foreach (string line in fileContent.Split("\n".ToCharArray()))
+            {
+                int currentDiff = temperatureDifferenceFromLine(line);
+                if (currentDiff < smallestDiff)
+                {
+                    smallestDiff = currentDiff;
+                    dayOfSmallestDiff = dayFromLine(line);
+                }
+            }
+            return dayOfSmallestDiff;
+        }
+    }
+}
