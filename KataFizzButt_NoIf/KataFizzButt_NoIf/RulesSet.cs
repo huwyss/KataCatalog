57c2b5786c42c56393a9a51ad35aa5ad4808d887
huwyss <hansueli.wyss@gmail.com>
2017-11-20 07:54:58 +0100

    extracted AddRule method in RulesSet

--- KataFizzButt_NoIf/KataFizzButt_NoIf/RulesSet.cs
+++ KataFizzButt_NoIf/KataFizzButt_NoIf/RulesSet.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace KataFizzButt_NoIf
 {
-    public class RulesSet 
+    public class RulesSet
     {
+        private ChainElement _firstRule = null;
         ChainElement _catchAllChainElement;
         ChainElement _fizzRuleChainElement;
         ChainElement _buzzRuleChainElement;
@@ -15,16 +16,28 @@ namespace KataFizzButt_NoIf
 
         public RulesSet()
         {
-            _catchAllChainElement = new ChainElement(new CatchAllRule(), null);
-            _fizzRuleChainElement = new ChainElement(new FizzRule(), _catchAllChainElement);
-            _buzzRuleChainElement = new ChainElement(new BuzzRule(), _fizzRuleChainElement);
-            _fizzBuzzRuleChainElement = new ChainElement(new FizzBuzzRule(), _buzzRuleChainElement);
+            AddRule(new CatchAllRule());
+            AddRule(new FizzRule());
+            AddRule(new BuzzRule());
+            AddRule(new FizzBuzzRule());
+
+
+            //_catchAllChainElement = new ChainElement(new CatchAllRule(), null);
+            //_fizzRuleChainElement = new ChainElement(new FizzRule(), _catchAllChainElement);
+            //_buzzRuleChainElement = new ChainElement(new BuzzRule(), _fizzRuleChainElement);
+            //_fizzBuzzRuleChainElement = new ChainElement(new FizzBuzzRule(), _buzzRuleChainElement);
         }
 
         public string ApplyRules(int number)
         {
-            string result = _fizzBuzzRuleChainElement.Handle(number);
+            string result = _firstRule.Handle(number);
             return result;
         }
+
+        private void AddRule(IRule rule)
+        {
+            _firstRule = new ChainElement(rule, _firstRule);
+        }
+
     }
 }
