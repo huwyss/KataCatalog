420ba0a8fe946b5e1697ca3e1e0c4f086145cc4f
huwyss <hansueli.wyss@gmail.com>
2017-11-20 07:58:49 +0100

    FizzBuzzer constructs RulesSet and adds Rules.

--- KataFizzButt_NoIf/KataFizzButt_NoIf/FizzBuzzer.cs
+++ KataFizzButt_NoIf/KataFizzButt_NoIf/FizzBuzzer.cs
@@ -8,11 +8,15 @@ namespace KataFizzButt_NoIf
 {
     public class FizzBuzzer
     {
-        RulesSet _rulesSet;
+        private readonly RulesSet _rulesSet;
 
         public FizzBuzzer()
         {
             _rulesSet = new RulesSet();
+            _rulesSet.AddRule(new CatchAllRule());
+            _rulesSet.AddRule(new FizzRule());
+            _rulesSet.AddRule(new BuzzRule());
+            _rulesSet.AddRule(new FizzBuzzRule());
         }
 
         public string Evaluate(int number)
--- KataFizzButt_NoIf/KataFizzButt_NoIf/RulesSet.cs
+++ KataFizzButt_NoIf/KataFizzButt_NoIf/RulesSet.cs
@@ -9,24 +9,6 @@ namespace KataFizzButt_NoIf
     public class RulesSet
     {
         private ChainElement _firstRule = null;
-        ChainElement _catchAllChainElement;
-        ChainElement _fizzRuleChainElement;
-        ChainElement _buzzRuleChainElement;
-        ChainElement _fizzBuzzRuleChainElement;
-
-        public RulesSet()
-        {
-            AddRule(new CatchAllRule());
-            AddRule(new FizzRule());
-            AddRule(new BuzzRule());
-            AddRule(new FizzBuzzRule());
-
-
-            //_catchAllChainElement = new ChainElement(new CatchAllRule(), null);
-            //_fizzRuleChainElement = new ChainElement(new FizzRule(), _catchAllChainElement);
-            //_buzzRuleChainElement = new ChainElement(new BuzzRule(), _fizzRuleChainElement);
-            //_fizzBuzzRuleChainElement = new ChainElement(new FizzBuzzRule(), _buzzRuleChainElement);
-        }
 
         public string ApplyRules(int number)
         {
@@ -34,10 +16,9 @@ namespace KataFizzButt_NoIf
             return result;
         }
 
-        private void AddRule(IRule rule)
+        public void AddRule(IRule rule)
         {
             _firstRule = new ChainElement(rule, _firstRule);
         }
-
     }
 }
